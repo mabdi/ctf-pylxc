d00a9a879080ff9b0b7446bdc7a1d35ef436594d
mabdi <newmrd@gmail.com>
2016-10-07 21:05:31 +0330

    apache init done

---
+++ challs/apach-test/on_init.py
@@ -0,0 +1,60 @@
+import lxc
+from os.path import join, isfile, exists
+import hashlib
+import json
+from distutils import dir_util
+
+LXC_NAME = "ide"
+MAIN_FOLDER = "/var/www/ide000"
+MAIN_FOLDER2 = "/var/www/apach"
+WEBROOT = "/var/www/html/ide"
+TEAMSALT = "6a204bd89f3c8348afd5c77c717a097a"
+
+def w_make_instance(arrr):
+   result = make_instance(arrr[0],arrr[1],arrr[2],arrr[3])
+   return result
+
+def make_instance(team,pid,folder,team_folder):
+   dest = WEBROOT + team_folder
+   if not exists(dest):
+       # create a codiad
+       dir_util.copy_tree(MAIN_FOLDER, dest )
+       with open(join(dest,"config.php")) as f:
+           newText=f.read()
+           newText=newText.replace("BASE_PATH_JA", dest )
+           newText=newText.replace("BASE_URL_JA", "ctf.behsazan.net/ide" + team_folder )
+       with open(join(dest,"config.php"), "w") as f:
+           f.write( newText )
+   dest2 = join(dest,'workspace','apache')
+   if exists(dest2):
+      return 10
+   # copy folder
+   dir_util.copy_tree(MAIN_FOLDER2, dest2 )
+   with open(join(dest,"data","projects.php")) as f:
+       jsonstr =f.read()
+       jsonstr = jsonstr[8:-5]
+       jsonstr = json.loads(jsonstr)   
+       jsonstr.append({'path':'apache','name':'apache'})
+       jsonstr = json.dumps(jsonstr)
+       
+   with open(join(dest,"data","projects.php"), "w") as f:
+       f.write( jsonstr )
+   return 11
+
+def init(team,pid,folder):
+   # access lxc
+   base = lxc.Container(LXC_NAME)
+   if not base.defined:
+      return {'stat': 0, 'msg':"container not defiend", 'data':None}
+   if not base.running:
+      return {'stat': 0, 'msg':"container not running", 'data':{'extra':"child cannot start. make sure you have `su - user` inside tmux and started the webservice"}}
+   # run a function inside it
+   team_folder = hashlib.md5((TEAMSALT+team).encode('utf-8')).hexdigest()
+   res = base.attach_wait(w_make_instance, [team, pid, folder, team_folder])
+   # return response
+   if res > 256:
+       res = int(res / 256 )
+   return {
+        10: {'stat': 1, 'msg':"already exists", 'data':{'redirect_to':"/ide" + team_folder}},
+        11: {'stat': 1, 'msg':"done", 'data':{'redirect_to':"/ide" + team_folder}},
+   }[res]
--- main.py
+++ main.py
@@ -10,6 +10,13 @@ import code
 app = Flask(__name__)
 app.debug = True
 
+ACCESS_TOKEN = "8e39a85022b8c194cb7cc3f7ae3e0217"
+
+CHALLS = {
+    "c9c3e050517cecff6c7bbc818d84ced7":"apach-test"
+
+}
+
 def mk_response(stat,msg,data=None):
     #print(json.dumps({'stat':stat,'msg':msg,'data':data}))
     return json.dumps({'stat':stat,'msg':msg,'data':data})
@@ -19,45 +26,29 @@ def load_py(path):
         return imp.load_source(path[:-3], path)
     except FileNotFoundError:
         print("Problem grader for {} is offline.".format(path))    
-		
-		
-@app.route("/api/onlevel", methods=['POST'])
-def on_level():
-     access_token = request.form.get('access_token', '').strip
-     folders = request.form.get('folders', '').strip()
-     pid = request.form.get('pid', '').strip()
-     if not access_token:
-        return mk_response(stat =0, msg = "access_denied")
-     if not folders:
-        return mk_response(stat = 0,msg ="درخواست نامعتبر")    
-     if not pid:
-        return mk_response(stat = 0,msg ="درخواست نامعتبر")
-     path = "./challs/{}/on_level.py".format(pid)
-     if not os.path.isfile(path):
-        return mk_response(stat = 0,msg ="شماره سوال نامعتبر")    
-     try:
-        result = load_py(path).on_level(pid,folders)
-        mk_response(stat =result['stat'],msg =result['msg'], data =result['data'])
-     except:
-        print("global Error") 
-    
-@app.route("/api/onteam", methods=['POST'])
-def on_team():
-     access_token = request.form.get('access_token', '').strip
+
+@app.route("/api/init", methods=['POST'])
+def on_init():
+     access_token = request.form.get('access_token', '').strip()
      folder = request.form.get('folder', '').strip()
+     team = request.form.get('team', '').strip()
      pid = request.form.get('pid', '').strip()
-     if not access_token:
+     if not access_token or access_token != ACCESS_TOKEN:
         return mk_response(stat =0, msg = "access_denied")
      if not folder:
         return mk_response(stat = 0,msg ="درخواست نامعتبر")    
+     if not team:
+        return mk_response(stat = 0,msg ="درخواست نامعتبر")        
      if not pid:
         return mk_response(stat = 0,msg ="درخواست نامعتبر")
-     path = "./challs/{}/on_team.py".format(pid)
+     pid = CHALLS[pid]
+     path = "./challs/{}/on_init.py".format(pid)
      if not os.path.isfile(path):
         return mk_response(stat = 0,msg ="شماره سوال نامعتبر")    
      try:
-        result = load_py(path).on_team(pid,folder)
-        mk_response(stat =result['stat'],msg =result['msg'], data =result['data'])
+        result = load_py(path).init(team,pid,folder)
+        print(result)
+        return mk_response(stat =result['stat'],msg =result['msg'], data =result['data'])
      except:
         print("global Error") 
 
@@ -77,7 +68,7 @@ def grade_flag():
      if not os.path.isfile(path):
         return mk_response(stat = 0,msg ="شماره سوال نامعتبر")
      try:
-        code.interact(local=locals()) 
+        #code.interact(local=locals()) 
         if key is not None:
            srcfold = "submits/" + folder + "/" + time.time()
            os.makedirs(srcfold)
@@ -86,7 +77,7 @@ def grade_flag():
            cmd = "cd "+srcfold+" && tar -zxf submit_src.tar"
            subprocess.call(cmd, shell=True)
         result = load_py(path).grade(folder,pid,key,srcfold,flg)
-        mk_response(stat =result['stat'],msg =result['msg'], data =result['data'])
+        return mk_response(stat =result['stat'],msg =result['msg'], data =result['data'])
      except:
         print("global Error") 
      
